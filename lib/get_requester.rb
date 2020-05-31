07cfac72e7780188dbe803bce59c03ac475ff61e
PARDEEP KAUR <pkaur84@aol.com>
2020-05-31 22:07:54 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,24 @@
-# Write your code here
+require 'net/http'
+
+class GetRequester
+
+  attr_accessor :url
+  
+  def initialize(url)
+    @url = url
+  end
+
+  def get_response_body
+    uri = URI.parse(@url)
+    res = Net::HTTP.get(uri)
+    JSON.parse(res)
+    
+  end
+  
+  def parse_json
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    JSON.parse(response)
+  end
+
+end 
